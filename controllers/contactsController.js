9a2fa8fcd941b09bebeae350b110b2a58bab4486
basseychrist <enobongetimbassey101@gmail.com>
2025-07-18 10:03:40 +0100

    week 2 done

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -31,7 +31,63 @@ const getContactById = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+    const db = getDb();
+    const newContact = req.body;
+
+    const result = await db.collection("contacts").insertOne(newContact);
+    res.status(201).json({ _id: result.insertedId, ...newContact });
+  } catch (err) {
+    console.error("Create Contact Error:", err);
+    res
+      .status(500)
+      .json({ message: "Error creating contact", error: err.message });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const db = getDb();
+    const id = req.params.id;
+    const updatedContact = req.body;
+
+    // Convert id to ObjectId
+    const result = await db
+      .collection("contacts")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updatedContact });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+    res.status(200).json({ message: "Contact updated successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error updating contact", error: err });
+  }
+};
+const deleteContact = async (req, res) => {
+  try {
+    const db = getDb();
+    const id = req.params.id;
+
+    // Convert id to ObjectId
+    const result = await db
+      .collection("contacts")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+    res.status(200).json({ message: "Contact deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting contact", error: err });
+  }
+};
+
 module.exports = {
   getAllContacts,
   getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const mongodb = require("./dataBase/db");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./docs/swagger.yaml");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 mongodb.initDb((err) => {
   if (err) {
@@ -12,12 +17,13 @@ mongodb.initDb((err) => {
   console.log("Connected to MongoDB");
 });
 
+app.use(bodyParser.json());
+app.use(cors());
 
 //Routes
 app.use("/", require("./routes"));
-
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`);
 });
